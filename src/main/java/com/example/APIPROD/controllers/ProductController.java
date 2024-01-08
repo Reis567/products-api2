86cb491ffa041441f7d2d9cdfe2cf8bd1fa1b62d
Reis567 <matheusdosreis9@gmail.com>
2024-01-08 20:00:34 -0300

    Delete method

--- src/main/java/com/example/APIPROD/controllers/ProductController.java
+++ src/main/java/com/example/APIPROD/controllers/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,4 +55,17 @@ public class ProductController {
     
         return ResponseEntity.status(HttpStatus.OK).body(product);
     }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable String id) {
+        Optional<Product> optionalProduct = repository.findById(id);
+
+        if (optionalProduct.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        repository.delete(optionalProduct.get());
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+}
+
 }
