d10c718cd27d5c1b48ef573f98abdde4991f5ae4
Reis567 <matheusdosreis9@gmail.com>
2024-01-08 08:10:08 -0300

    product repository and get all method

--- src/main/java/com/example/APIPROD/controllers/ProductController.java
+++ src/main/java/com/example/APIPROD/controllers/ProductController.java
@@ -1,15 +1,22 @@
 package com.example.APIPROD.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.APIPROD.domain.product.ProductRepository;
+
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    @Autowired
+    private ProductRepository repository;
+
     @GetMapping
     public ResponseEntity getAllProducts(){
-        return ResponseEntity.ok("oK");
+        var allProducts = repository.findAll();
+        return ResponseEntity.ok(allProducts);
     }
 }
