a8fcc473b7136aa9632c648f7094d82af3f3bed3
Reis567 <matheusdosreis9@gmail.com>
2024-01-08 08:01:59 -0300

    Product entity

---
+++ src/main/java/com/example/APIPROD/domain/product/Product.java
@@ -0,0 +1,26 @@
+package com.example.APIPROD.domain.product;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name="PRODUCT")
+@Entity(name = "PRODUCT")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Number price_in_cents;
+}
