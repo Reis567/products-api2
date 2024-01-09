6645725a0dea3a7d86b97a6f9521b1bf2d020f5b
Reis567 <matheusdosreis9@gmail.com>
2024-01-09 07:56:06 -0300

    authentication manager and BCryot instance

--- src/main/java/com/example/APIPROD/controllers/AuthenticationController.java
+++ src/main/java/com/example/APIPROD/controllers/AuthenticationController.java
@@ -1,6 +1,10 @@
 package com.example.APIPROD.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +17,13 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data){
-        
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
