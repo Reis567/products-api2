03561d81c3d9386ea60c9571b09000fe018136ce
Reis567 <matheusdosreis9@gmail.com>
2024-01-09 19:20:12 -0300

    filter to check the integrity of token

--- src/main/java/com/example/APIPROD/infra/security/SecurityConfiguration.java
+++ src/main/java/com/example/APIPROD/infra/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -26,6 +27,7 @@ public class SecurityConfiguration {
                                                     .anyRequest().authenticated()
 
                                 )
+                                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                             .build();
     }
     @Bean
