4e75663c0ad993c1f5f2635bc4d1844a6dfb5ead
Reis567 <matheusdosreis9@gmail.com>
2024-01-09 07:28:14 -0300

    bean anottation on security filter chain

--- src/main/java/com/example/APIPROD/domain/user/User.java
+++ src/main/java/com/example/APIPROD/domain/user/User.java
@@ -8,6 +8,9 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -24,6 +27,8 @@ import lombok.Setter;
 @EqualsAndHashCode(of = "id")
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     private String login;
